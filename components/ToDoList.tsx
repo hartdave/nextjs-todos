fbe535ba7e3ba8374fac2abc1c87db63e4a0f2e6
dah7968 <dhart@hbs.edu>
2022-10-25 13:46:40 -0700

    clean up from vanilla js solutions

--- components/ToDoList.tsx
+++ components/ToDoList.tsx
@@ -1,22 +1,61 @@
 import * as React from 'react';
 
-export interface IAppProps {
-}
+if (typeof window !== 'undefined') {
+const todoForm = document.getElementById('todo-form');
+const todoInput = document.getElementById('todo-input');
+const todoSubmit = document.getElementById('todo-submit');
+const todoList = document.getElementById('todo-list');
 
-export function ToDoList (props: IAppProps) {
+const deleteItem = (btn) => {
+    let elem = btn.parentNode;
+    const index = [...elem.parentElement.children].indexOf(elem);
+    todoStorage.splice(index, 1);
+    localStorage.setItem('todoList', JSON.stringify(todoStorage));
+    elem.remove();
+};
+
+const buildList = (text) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = 
+        `<div className='item-description'>` +
+            text +
+        '</div><div className="item-button-group"><button className="edit-button">Edit</button> | <button onclick="deleteItem(this)">Delete</button></div>';
+    todoList.appendChild(listItem)
+};
+
+let todoStorage = localStorage.getItem('todo-item')
+    ? JSON.parse(localStorage.getItem('todo-item')): [];
+
+todoForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    todoStorage.push(todoInput.value);
+    localStorage.setItem('todo-item', JSON.stringify(todoStorage));
+    buildList(todoInput.value);
+    todoInput.value = "";
+});
+
+
+};
+
+
+
+
+
+
+export function ToDoList () {
   return (
     <div className='container'>
         <h1 className="text-3xl font-bold">
         Welcome to the to do app
         </h1>
-        <form className='todo-form'>
-            <input type="text" className="todo-input" placeholder='Add a to do item' />
-            <button type="submit" className='add-button'>Add</button>
+        <form className='todo-form' id="todo-form">
+            <input type="text" className="todo-input" id="todo-input" placeholder='Add a to do item' />
+            <button type="submit" className='add-button' id="todo-submit">Add</button>
         </form>
         <div className='todo-items'>
             <h2 className="text-2xl font-bold">To Do List:</h2>
-            <ul>
-                <li className='item' uid='9999999999999'>
+            <ul id="todo-list">
+                <li>
                     <div className='item-description'>
                         Test item
                     </div>
--- pages/api/swapi.ts
+++ pages/api/swapi.ts
@@ -7,13 +7,7 @@ interface Swapi {
     results: Array<Person>
 }
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
-    const response = fetch('https://swapi.dev/api/people/');
-    const data: Swapi = res.json();
-    const random = Math.floor(Math.random()*(json.results.length - 1));
-    const person: Person = json.results[random];
-    res.status(200).json(person);
-}
+
 
 
   
