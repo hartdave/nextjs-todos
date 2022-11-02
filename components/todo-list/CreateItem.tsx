ca44737dd470a52dd046efe1b66383bf01a5265b
dah7968 <dhart@hbs.edu>
2022-11-02 08:21:25 -0700

    format cleanup, prettier

--- components/todo-list/CreateItem.tsx
+++ components/todo-list/CreateItem.tsx
@@ -1,38 +1,53 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 export const dataArray: Array<object> = [];
 export const Form = () => {
-    
-    const initialState: {id: string, todo: string} = {
-        id: "",
-        todo: "",
-    }
-    const [createData, setCreateData] = useState(initialState);
-    
-    const createToDo = () => {
-        if (createData  === initialState) {
-            return alert("Please Enter a To Do Item");
-        } else {
-            createData.id = new Date().toUTCString();
-            let newLocal: string = window.localStorage.getItem("ToDos");
-            let newArray: Array<object> = JSON.parse(newLocal);
-            if (newArray == null) {
-                dataArray.push(createData);
-            } else {
-            dataArray.push(...newArray, createData);
-            };
-            window.localStorage.setItem("ToDos", JSON.stringify(dataArray));
-            
-        }
-    }
+  const initialState: { id: string; todo: string } = {
+    id: "",
+    todo: "",
+  };
+  const [createData, setCreateData] = useState(initialState);
 
-    return (
-        
-        <form className='todo-form' id="todo-form" onSubmit={createToDo}>
-            <div className='flex flex-row justify-between border-b border-green-500 px-1 py-2'>
-            <input type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none" placeholder='Add a to do item' onChange={(e) => setCreateData({ ...createData, todo: e.target.value })} value={createData.todo} />
-            <button type="submit" className='flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded' id="todo-submit">Add</button>
-            </div>
-        </form>
-    );
+  const createToDo = () => {
+    if (createData === initialState) {
+      return alert("Please Enter a To Do Item");
+    } else {
+      createData.id = new Date().toUTCString();
+      let newLocal: string = window.localStorage.getItem("ToDos");
+      let newArray: Array<object> = JSON.parse(newLocal);
+      if (newArray == null) {
+        dataArray.push(createData);
+      } else {
+        dataArray.push(...newArray, createData);
+      }
+      window.localStorage.setItem("ToDos", JSON.stringify(dataArray));
+    }
+  };
+
+  return (
+    <form
+      className='todo-form'
+      id='todo-form'
+      onSubmit={createToDo}
+    >
+      <div className='flex flex-row justify-between border-b border-green-500 px-1 py-2'>
+        <input
+          type='text'
+          className='appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 focus:outline-none'
+          placeholder='Add a to do item'
+          onChange={(e) =>
+            setCreateData({ ...createData, todo: e.target.value })
+          }
+          value={createData.todo}
+        />
+        <button
+          type='submit'
+          className='flex-shrink-0 bg-green-500 hover:bg-green-700 border-green-500 hover:border-green-700 text-sm border-4 text-white py-1 px-2 rounded'
+          id='todo-submit'
+        >
+          Add
+        </button>
+      </div>
+    </form>
+  );
 };
--- components/todo-list/List.tsx
+++ components/todo-list/List.tsx
@@ -1,76 +1,99 @@
-import { useState, useEffect } from 'react';
-import { FaEdit, FaTrash } from 'react-icons/fa';
-
-
+import { useState, useEffect } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 export const List = () => {
-    const [ todos, setTodos] = useState([]);
-    const [ editing, setEditing] = useState("");
-    const [ editData, setEditData] = useState({todo: ""})
+  const [todos, setTodos] = useState([]);
+  const [editing, setEditing] = useState("");
+  const [editData, setEditData] = useState({ todo: "" });
 
+  useEffect(() => {
+    const listData: Array<object> = JSON.parse(localStorage.getItem("ToDos"));
 
-    useEffect(() => {
-        const listData: Array<object> = JSON.parse(localStorage.getItem('ToDos'));
-      
-        if (listData !== null) {
-          setTodos(listData);
-        }
-      }, []);
-    
-      const deleteTodo = (id: string) => {
-        const remainingTodos: Array<string> = todos.filter(todo => id !== todo.id);
-        setTodos(remainingTodos);
-        updateLocalStorage(remainingTodos);
-      }
+    if (listData !== null) {
+      setTodos(listData);
+    }
+  }, []);
 
-      const changeEditing = (id: string) => {
-        setEditing(() => id);
-        };
+  const deleteTodo = (id: string) => {
+    const remainingTodos: Array<string> = todos.filter(
+      (todo) => id !== todo.id
+    );
+    setTodos(remainingTodos);
+    updateLocalStorage(remainingTodos);
+  };
 
-      const updateTodo = (val: string, id: string) => {
-        const index = todos.findIndex(object => {
-            return object.id === id;
-        });
-        if (index !== -1) {
-            todos[index].todo = val;
-            updateLocalStorage(todos);
-            changeEditing("");
-            setEditData({todo: ""});
-        }
-      }
-      
-      const updateLocalStorage = (todoList: Array<string>) => {
-        localStorage.setItem('ToDos', JSON.stringify(todoList));
-      };
+  const changeEditing = (id: string) => {
+    setEditing(() => id);
+  };
 
-    return (
-        <>  
-            {todos.map((todo) => {
-                return (
-                    <li key={todo.id} className="mt-2">
-                        
-                            {editing === todo.id ? 
-                              
-                                 <form onSubmit={() => updateTodo(editData.todo, todo.id)}>
-                                    <div className='flex flex-row justify-between border border-green-600'>
-                                    <input type="text" className="appearance-none block  border-none w-full text-gray-700 pb-1 focus:outline-none mr-5" placeholder={todo.todo} onChange={(e) => setEditData({ ...todo.todo, todo: e.target.value })} value={editData.todo} id={todo.id} />
-                                    <button type='submit'className=' hover:text-green-600'><FaEdit /></button>
-                                    </div>
-                                </form>
-                              
-                            :
-                            <div className='flex flex-row justify-between'>
-                              <div className='text-lg'>{todo.todo}</div>
-                              <div>
-                              <button className='hover:text-red-600' onClick={() => deleteTodo(todo.id)}><FaTrash /></button> | <button className='hover:text-green-600'onClick={() => changeEditing(todo.id)}><FaEdit /></button>
-                              </div>
-                            </div>
-                            }
-                        
-                        
-                    </li>
-                )
-            })}
-        </>
-    )
+  const updateTodo = (val: string, id: string) => {
+    const index = todos.findIndex((object) => {
+      return object.id === id;
+    });
+    if (index !== -1) {
+      todos[index].todo = val;
+      updateLocalStorage(todos);
+      changeEditing("");
+      setEditData({ todo: "" });
+    }
+  };
+
+  const updateLocalStorage = (todoList: Array<string>) => {
+    localStorage.setItem("ToDos", JSON.stringify(todoList));
+  };
+
+  return (
+    <>
+      {todos.map((todo) => {
+        return (
+          <li
+            key={todo.id}
+            className='mt-2'
+          >
+            {editing === todo.id ? (
+              <form onSubmit={() => updateTodo(editData.todo, todo.id)}>
+                <div className='flex flex-row justify-between border border-green-600'>
+                  <input
+                    type='text'
+                    className='appearance-none block  border-none w-full text-gray-700 pb-1 focus:outline-none mr-5'
+                    placeholder={todo.todo}
+                    onChange={(e) =>
+                      setEditData({ ...todo.todo, todo: e.target.value })
+                    }
+                    value={editData.todo}
+                    id={todo.id}
+                  />
+                  <button
+                    type='submit'
+                    className=' hover:text-green-600'
+                  >
+                    <FaEdit />
+                  </button>
+                </div>
+              </form>
+            ) : (
+              <div className='flex flex-row justify-between'>
+                <div className='text-lg'>{todo.todo}</div>
+                <div>
+                  <button
+                    className='hover:text-red-600'
+                    onClick={() => deleteTodo(todo.id)}
+                  >
+                    <FaTrash />
+                  </button>{" "}
+                  |{" "}
+                  <button
+                    className='hover:text-green-600'
+                    onClick={() => changeEditing(todo.id)}
+                  >
+                    <FaEdit />
+                  </button>
+                </div>
+              </div>
+            )}
+          </li>
+        );
+      })}
+    </>
+  );
 };
